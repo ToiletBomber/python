b7514fe2196977e64485b1b0e295df9b1f9c6f2a
tcmix <793069303@qq.com>
2019-03-29 13:49:32 +0800

    frame

--- Tkinter/10_menubar.py
+++ Tkinter/10_menubar.py
@@ -11,14 +11,38 @@ window = tk.Tk()
 window.title('My window')
 window.geometry('500x300')
 
-l = tk.Label(window, bg='green', text= '         ')
+l = tk.Label(window, bg='green',fg='white' ,text= '         ')
 l.pack()
+counter = 0
+def do_job():
+    global counter
+    l.config(text='do ' + str(counter))
+    counter += 1
+#----------------------------------------------------------------------
 #创建一个菜单栏。可理解为一个容器，在窗口上方
 menubar = tk.Menu(window)
 #创建一个file菜单项（默认不下拉。下拉内容包括New,Open,Save,Exit）
-filemenu = tk.Menu(menubar, tearoff=0)
-#将定义的空菜单命名为file，
+filemenu = tk.Menu(menubar, tearoff=0)#tearoff=0菜单不能独立
+#将定义的空菜单命名为file，放到菜单栏上，就是哪个容器
 menubar.add_cascade(label='File', menu=filemenu)
+#在filemenu上添加菜单
+filemenu.add_command(label='New',command=do_job)
+filemenu.add_command(label='Open',command=do_job)
+filemenu.add_command(label='Save',command=do_job)
+filemenu.add_separator()#分割线
+filemenu.add_command(label='Exit',command=window.quit)
+
+#----------------------------------------------------------------------
+editmenu = tk.Menu(menubar, tearoff=0)#二级菜单
+menubar.add_cascade(label='Edit', menu=editmenu, underline=0)
+editmenu.add_command(label='Cut', command=do_job)
+editmenu.add_command(label='Copy', command=do_job)
+editmenu.add_command(label='Paste', command=do_job)
+
+submenu = tk.Menu(filemenu)
+filemenu.add_cascade(label='Import', command=do_job)
+
+#----------------------------------------------------------------------
 #创建完菜单栏，配置让菜单栏显示出来
 window.config(menu=menubar)
 
---
+++ Tkinter/11_frame.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 11:27:11 2019
+
+@author: Administrator
+"""
+
+import tkinter as tk
+
+window = tk.Tk()
+
+window.title('My window')
+window.geometry('500x300')
+tk.Label(window, text='on the window', bg='red',font=('Arial', 16)).pack()
+
+#创建一个主frame，在主window窗口上
+frame = tk.Frame(window)
+frame.pack()
+#创建一个frame_l，在frame窗口上
+frame_l = tk.Frame(frame)
+frame_r = tk.Frame(frame)
+frame_l.pack(side='left')
+frame_r.pack(side='right')
+
+tk.Label(frame_l, text='on the frame_l1', bg='green').pack()
+tk.Label(frame_l, text='on the frame_l2', bg='green').pack()
+tk.Label(frame_l, text='on the frame_l3', bg='green').pack()
+tk.Label(frame_r, text='on the frame_r1', bg='yellow').pack()
+tk.Label(frame_r, text='on the frame_r1', bg='yellow').pack()
+tk.Label(frame_r, text='on the frame_r3', bg='yellow').pack()
+
+window.mainloop()
